6675582480066127fd02bc253635b45876a59b75
hash1000 <hashirmughal1000@gmail.com>
2023-09-07 03:11:22 +0500

    update commit

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,20 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 
 function UserList() {
-  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     // Fetch data from your Flask API
     axios.get('http://127.0.0.1:5000/users')
       .then(response => {
-        setUsers(response.data);
-        console.log(users);
+        console.log(response.data);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
-  }, []);
+  }, []);  
 
   return (
     <div>
